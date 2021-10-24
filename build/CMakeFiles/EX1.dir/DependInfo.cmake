bfad9ef1d63cfb0970f741750b14d0ae44bbe2ba
David Gurovich <davidgurovich580@gmail.com>
2021-10-24 06:11:19 -0700

    first_hw_david_commit

---
+++ build/CMakeFiles/EX1.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/david1998/Documents/HW/tihnut_mitkadem1/ex1/MainTrain.cpp" "/home/david1998/Documents/HW/tihnut_mitkadem1/ex1/build/CMakeFiles/EX1.dir/MainTrain.cpp.o"
+  "/home/david1998/Documents/HW/tihnut_mitkadem1/ex1/anomaly_detection_util.cpp" "/home/david1998/Documents/HW/tihnut_mitkadem1/ex1/build/CMakeFiles/EX1.dir/anomaly_detection_util.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/david1998/Documents/HW/tihnut_mitkadem1/ex1
+# Build directory: /home/david1998/Documents/HW/tihnut_mitkadem1/ex1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
